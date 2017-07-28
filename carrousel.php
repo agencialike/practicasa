a70256b790e388934327d5385f9fc1ce16f3fc2e
Jorge Robalino <jrobalino@like.com.ecm>
2017-07-28 15:37:20 -0500

    carrousel

--- carrousel.php
+++ carrousel.php
@@ -1,74 +1,85 @@
+
 <style>
 .carousel-caption{
     color: #6A6A6A;
     text-shadow:none;
     font-size: 50px;
+    text-align: right;
 }
-.carousel-caption span{
+.carousel-caption h2{
+    font-size: 50px;
+    font-weight: 800;
+}
+.carousel-caption p{
     margin-top: 20px;
     font-size: 16px;
     line-height: 16px;
 }
+.ctacarr{
+    background: #636363;
+    text-align: center;
+    font-size: 18px;
+    padding: 10px;
+    color: white;
+    margin-top: 15px;
+    transition-duration: .5s;
+}
+.ctacarr:hover{
+    background: #FFB427;
+    text-align: center;
+    font-size: 18px;
+    padding: 10px;
+    color: white;
+    margin-top: 15px;
+}
+
 </style>
 
-<!-- Full Page Image Background Carousel Header -->
-    <header id="myCarousel" class="carousel slide">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1" style="background: #C4C4C4;"></li>
-            <li data-target="#myCarousel" data-slide-to="2" style="background: #C4C4C4;"></li>
-        </ol>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
-        <!-- Wrapper for Slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <!-- Set the first background image using inline CSS below. -->
-                <div class="fill" style=""></div>
-                    <div class="carousel-caption">
-                        <div class="col-md-6 col-xs-12">
-                            <h2 class="text-cap">Línea residencial &  Comercial. <br> <br>
-                           <span> Nuestra linea de productos esta dirigido a clientes que buscan una alta calidad y diseño exclusivo.</span></h2>
-                        </div>
-                        <div class="col-md-6 col-xs-12">
-                           <img src="http://localhost/wordpress/wp-content/uploads/2017/07/Captura-de-pantalla-2017-07-28-a-las-9.50.16-a.m..png" alt="">
-                        </div>
-                    </div>
-                </div>
-            <div class="item">
-                <!-- Set the second background image using inline CSS below. -->
-                <div class="fill"></div>
-                <div class="carousel-caption">
-                    <div class="col-md-6 col-xs-12">
-                            <h2>Caption 1</h2>
-                        </div>
-                        <div class="col-md-6 col-xs-12">
-                            <img src="http://localhost/wordpress/wp-content/uploads/2017/07/Captura-de-pantalla-2017-07-28-a-las-9.50.16-a.m..png" alt="">
-                        </div>
-                </div>
-            </div>
-            <div class="item">
-                <!-- Set the third background image using inline CSS below. -->
-                <div class="fill"></div>
-                <div class="carousel-caption">
-                    <div class="col-md-6 col-xs-12">
-                            <h2>Caption 1</h2>
-                        </div>
-                        <div class="col-md-6 col-xs-12">
-                            <img src="http://localhost/wordpress/wp-content/uploads/2017/07/Captura-de-pantalla-2017-07-28-a-las-9.50.16-a.m..png" alt="">
-                        </div>
-                </div>
-            </div>
-        </div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+    <div class="carousel-caption">
+        <h2 class="text-cap">Línea residencial &  Comercial. </h2>
+        <p style="line-height: 20px;"> Nuestra linea de productos esta dirigido a clientes que buscan una alta calidad y diseño exclusivo.</p>
+        <a href=""><div class="col-md-3 col-md-offset-9 col-xs-12 ctacarr">Ver más</div></a>
+      </div>
+      <img src="http://localhost/wordpress/wp-content/uploads/2017/07/Captura-de-pantalla-2017-07-28-a-las-9.50.27-a.m..png" alt="Los Angeles">
+    </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="icon-prev"></span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="icon-next"></span>
-        </a>
+    <div class="item">
+    <div class="carousel-caption">
+        <h2 class="text-cap">Línea Industrial. </h2>
+        <p style="line-height: 20px;"> Nuestra línea industrial se enfoca en dar soluciones a problemas <br> energeticos, de temperatura, ahorro de energia, tanto como en industrias, aeropuertos, mercados <br>bry galpones. Damos soluciones a problema en desgaste de pisos para industrias, bodegas y parqueos.</p>
+        <a href=""><div class="col-md-3 col-md-offset-9 col-xs-12 ctacarr">Ver más</div></a>
+      </div>
+      <img src="http://localhost/wordpress/wp-content/uploads/2017/07/Captura-de-pantalla-2017-07-28-a-las-9.50.27-a.m..png" alt="Chicago">
+    </div>
 
-    </header>
+    <div class="item">
+    <div class="carousel-caption">
+        <h2 class="text-cap">Línea de Mantenimiento. </h2>
+        <p style="line-height: 20px;"> Productos especializados en la limpieza antes y después de la <br>instalación de las obras para porcelanatos, marmoles y granitos.</p>
+        <a href=""><div class="col-md-3 col-md-offset-9 col-xs-12 ctacarr">Ver más</div></a>
+      </div>
+      <img src="http://localhost/wordpress/wp-content/uploads/2017/07/Captura-de-pantalla-2017-07-28-a-las-9.50.27-a.m..png" alt="New York">
+    </div>
+  </div>
 
-    <!-- Page Content -->
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
